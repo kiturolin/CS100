08e6c6bd398d334e5565c43415ee06a59c3edc7c
kiturolin <kiturolin@gmail.com>
2025-04-29 23:52:49 +0800

    halfway through ScoreBoard

--- PA/PA6/prob3/ScoreBoard_hint1.hpp
+++ PA/PA6/prob3/ScoreBoard_hint1.hpp
@@ -6,90 +6,129 @@
 
 inline constexpr auto MSPerTick = 50;
 
-namespace detail {
+namespace detail
+{
+enum class PadLayout { Horizontal, Vertical };
 
 template <std::size_t Height, std::size_t Width>
-static void printBlock(const char (&object)[Height][Width], int baseRow,
-                       int baseCol) {
+static void
+printBlock (const char (&object)[Height][Width], int baseRow, int baseCol)
+{
   for (int row = 0; row != Height; ++row) {
-    move_cursor(baseRow + row, baseCol);
+    move_cursor (baseRow + row, baseCol);
     std::cout << object[row];
   }
   std::cout << std::flush;
 }
 
-} // namespace detail
+}    // namespace detail
 
-class DigitPad {
+class DigitPad
+{
+public:
+  enum class PadType { Games, Points };
+
+private:
   const TextColor mColor;
   const int mBaseRow;
   const int mBaseCol;
+  const int mNo;    // 第几块digitalPad, 从0开始计数
+  const PadType mPadType;
   int mCurrentDigit;
 
 public:
-  DigitPad(TextColor color, int baseRow, int baseCol);
+  DigitPad (TextColor color, int no, PadType padType, int baseRow, int baseCol);
 
-  int getBaseRow() const;
+  int getBaseRow () const { return mBaseRow; };
 
-  int getBaseCol() const;
+  int getBaseCol () const { return mBaseCol; };
 
-  int getDigit() const;
+  int getDigit () const { return mCurrentDigit; };
 
-  void print() const;
+  void print () const;
 
   enum class ScrollDirection { Up, Down };
 
-  void updateAndScroll(int newDigit, ScrollDirection scrollDir);
+  void updateAndScroll (int newDigit, ScrollDirection scrollDir);
 };
 
-class Player {
+class Player
+{
+  static constexpr int NUM_PADS = 2;
   std::string mName;
+  detail::PadLayout mPadLayout;
   DigitPad mTensPlace;
   DigitPad mOnesPlace;
 
 public:
   enum class Side { Left, Right };
 
-  Player(std::string name, Side side)
-      : mName{std::move(name)},
-        mTensPlace{ColorOfSide(side), 0, BaseColOfSide(side)},
-        mOnesPlace{ColorOfSide(side), 0, BaseColOfSide(side) + FrameWidth} {}
+  Player (std::string name, detail::PadLayout layout, Side side) :
+      mName{ std::move (name) },
+      mPadLayout{ layout },
+      mTensPlace{
+	ColorOfSide (side), 0, DigitPad::PadType::Points, BaseRowOfSide (layout, side), BaseColOfPad (0, layout, side)
+      },
+      mOnesPlace{
+	ColorOfSide (side), 1, DigitPad::PadType::Points, BaseRowOfSide (layout, side), BaseColOfPad (1, layout, side)
+      }
+  {
+  }
 
-  int getScore() const;
+  int getScore () const;
 
-  void print() const;
+  void print () const;
 
-  void updateScore(int delta);
+  void updateScore (int delta);
 
 private:
-  static TextColor ColorOfSide(Side side);
+  static TextColor ColorOfSide (Side side);
 
-  static int BaseColOfSide(Side side);
+  static int BaseColOfPad (const int no, detail::PadLayout layout, Side side)
+  {
+    // 如果是水平排列的记分板
+    if (layout == detail::PadLayout::Horizontal)
+      return side == Side::Left ? no * FrameWidth : no * FrameWidth + NUM_PADS;
+    return no * FrameWidth;
+  };
+
+  static int BaseRowOfSide (detail::PadLayout layout, Side side)
+  {
+    if (layout == detail::PadLayout::Vertical) return side == Side::Left ? 0 : FrameHeight;
+    return 0;
+  }
 };
 
-class ScoreBoard {
+class ScoreBoard
+{
   Player mLeft;
   Player mRight;
 
 public:
-  ScoreBoard(std::string leftName, std::string rightName)
-      : mLeft{std::move(leftName), Player::Side::Left},
-        mRight{std::move(rightName), Player::Side::Right} {
+  ScoreBoard (std::string leftName, std::string rightName, detail::PadLayout layout) :
+      mLeft{ std::move (leftName), layout, Player::Side::Left },
+      mRight{ std::move (rightName), layout, Player::Side::Right }
+  {
     // TODO:
   }
 
-  void leftInc(int delta = 1) { mLeft.updateScore(delta); }
-  void rightInc(int delta = 1) { mRight.updateScore(delta); }
-  void leftDec(int delta = 1) { mLeft.updateScore(-delta); }
-  void rightDec(int delta = 1) { mRight.updateScore(-delta); }
+  void leftInc (int delta = 1) { mLeft.updateScore (delta); }
 
-  ~ScoreBoard() {
+  void rightInc (int delta = 1) { mRight.updateScore (delta); }
+
+  void leftDec (int delta = 1) { mLeft.updateScore (-delta); }
+
+  void rightDec (int delta = 1) { mRight.updateScore (-delta); }
+
+  ~ScoreBoard ()
+  {
     for (int row = FrameHeight; row >= 0; --row) {
-      move_cursor(row, 0);
-      clear_to_eol();
-      sleep_ms(MSPerTick);
+      move_cursor (row, 0);
+      clear_to_eol ();
+      sleep_ms (MSPerTick);
     }
   }
 };
 
-#endif // SCOREBOARD_HPP
+#endif	  // SCOREBOARD_HPP
+
--- PA/PA6/prob3/utils.hpp
+++ PA/PA6/prob3/utils.hpp
@@ -2,146 +2,164 @@
 #define SCOREBOARD_UTILS_HPP
 
 #if defined(__MINGW64_VERSION_MAJOR) && __MINGW64_VERSION_MAJOR < 7
-#error Your MinGW version is too old. Go to winlibs.com to install the latest MinGW-w64.
+  #error Your MinGW version is too old. Go to winlibs.com to install the latest MinGW-w64.
 #endif
 
 #include <iostream>
 
 #ifdef _WIN32
-#include <conio.h>
-#include <windows.h>
+  #include <conio.h>
+  #include <windows.h>
 #else
-#include <fcntl.h>
-#include <termios.h>
-#include <unistd.h>
-#endif // _WIN32
+  #include <fcntl.h>
+  #include <termios.h>
+  #include <unistd.h>
+#endif	  // _WIN32
 
 struct PrinterGuard {
-  PrinterGuard() {
+  PrinterGuard ()
+  {
     if (numOfInstances++ == 0) {
-      hide_cursor();
-      disable_input_echo();
+      hide_cursor ();
+      disable_input_echo ();
     }
   }
-  ~PrinterGuard() {
+
+  ~PrinterGuard ()
+  {
     if (--numOfInstances == 0) {
-      show_cursor();
-      enable_input_echo();
+      show_cursor ();
+      enable_input_echo ();
     }
   }
 
 private:
   static int numOfInstances;
 
-  static void hide_cursor() { std::cout << "\033[?25l" << std::flush; }
+  static void hide_cursor () { std::cout << "\033[?25l" << std::flush; }
 
-  static void show_cursor() { std::cout << "\033[?25h" << std::flush; }
+  static void show_cursor () { std::cout << "\033[?25h" << std::flush; }
 
-  static void disable_input_echo() {
+  static void disable_input_echo ()
+  {
 #ifdef _WIN32
-    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD mode = 0;
-    GetConsoleMode(hStdin, &mode);
-    SetConsoleMode(hStdin, mode & ~ENABLE_ECHO_INPUT);
+    HANDLE hStdin = GetStdHandle (STD_INPUT_HANDLE);
+    DWORD mode	  = 0;
+    GetConsoleMode (hStdin, &mode);
+    SetConsoleMode (hStdin, mode & ~ENABLE_ECHO_INPUT);
 #else
     termios t;
-    tcgetattr(STDIN_FILENO, &t);
+    tcgetattr (STDIN_FILENO, &t);
     t.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &t);
-#endif // _WIN32
+    tcsetattr (STDIN_FILENO, TCSANOW, &t);
+#endif	  // _WIN32
   }
 
-  static void enable_input_echo() {
+  static void enable_input_echo ()
+  {
 #ifdef _WIN32
-    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD mode = 0;
-    GetConsoleMode(hStdin, &mode);
-    SetConsoleMode(hStdin, mode | ENABLE_ECHO_INPUT);
+    HANDLE hStdin = GetStdHandle (STD_INPUT_HANDLE);
+    DWORD mode	  = 0;
+    GetConsoleMode (hStdin, &mode);
+    SetConsoleMode (hStdin, mode | ENABLE_ECHO_INPUT);
 #else
     termios t;
-    tcgetattr(STDIN_FILENO, &t);
+    tcgetattr (STDIN_FILENO, &t);
     t.c_lflag |= ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &t);
-#endif // _WIN32
+    tcsetattr (STDIN_FILENO, TCSANOW, &t);
+#endif	  // _WIN32
   }
 };
 
 int PrinterGuard::numOfInstances = 0;
 
-void move_cursor(int row, int col) {
+void
+move_cursor (int row, int col)
+{
   std::cout << "\033[" << row + 1 << ';' << col + 1 << 'f' << std::flush;
 }
 
-void clear_screen() {
+void
+clear_screen ()
+{
 #ifdef _WIN32
-  std::system("cls");
+  std::system ("cls");
 #else
   std::cout << "\033[2J\033[1;1f" << std::flush;
-#endif // _WIN32
+#endif	  // _WIN32
 }
 
-void clear_to_eol() { std::cout << "\033[0K" << std::flush; }
+void
+clear_to_eol ()
+{
+  std::cout << "\033[0K" << std::flush;
+}
 
-void sleep_ms(long long t) {
+void
+sleep_ms (long long t)
+{
 #ifdef _WIN32
-  Sleep(t);
+  Sleep (t);
 #else
-  usleep(t * 1000);
-#endif // _WIN32
+  usleep (t * 1000);
+#endif	  // _WIN32
 }
 
 enum class TextColor { Normal, Red, Blue };
 
-std::ostream &operator<<(std::ostream &os, TextColor color) {
-  static constexpr const char *Escapes[] = {"\033[0m", "\033[31m", "\033[34m"};
-  os << Escapes[static_cast<int>(color)] << std::flush;
+std::ostream &
+operator<< (std::ostream &os, TextColor color)
+{
+  static constexpr const char *Escapes[] = { "\033[0m", "\033[31m", "\033[34m" };
+  os << Escapes[static_cast<int> (color)] << std::flush;
   return os;
 }
 
 struct ColorGuard {
-  ColorGuard(TextColor color) { std::cout << color << std::flush; }
-  ~ColorGuard() { std::cout << TextColor::Normal << std::flush; }
+  ColorGuard (TextColor color) { std::cout << color << std::flush; }
+
+  ~ColorGuard () { std::cout << TextColor::Normal << std::flush; }
 };
 
-namespace detail {
+namespace detail
+{
 
 struct Starter {
-  Starter() {
-    if (!stdout_is_terminal()) {
+  Starter ()
+  {
+    if (!stdout_is_terminal ()) {
       std::cerr << "ScoreBoard must be run in a terminal.\n";
-      exit(EXIT_FAILURE);
+      exit (EXIT_FAILURE);
     }
 #ifdef _WIN32
-    HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hStd == INVALID_HANDLE_VALUE) {
-      exit(EXIT_FAILURE);
-    }
+    HANDLE hStd = GetStdHandle (STD_OUTPUT_HANDLE);
+    if (hStd == INVALID_HANDLE_VALUE) { exit (EXIT_FAILURE); }
     DWORD dwMode = 0;
-    if (!GetConsoleMode(hStd, &dwMode)) {
-      exit(EXIT_FAILURE);
-    }
+    if (!GetConsoleMode (hStd, &dwMode)) { exit (EXIT_FAILURE); }
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    if (!SetConsoleMode(hStd, dwMode)) {
+    if (!SetConsoleMode (hStd, dwMode)) {
       std::cerr << "Can't enable virtual terminal processing.\n";
-      exit(EXIT_FAILURE);
+      exit (EXIT_FAILURE);
     }
-#endif // _WIN32
+#endif	  // _WIN32
   }
 
   static const Starter starter;
 
 private:
-  static bool stdout_is_terminal() {
+  static bool stdout_is_terminal ()
+  {
 #ifdef _WIN32
-    return GetFileType(GetStdHandle(STD_OUTPUT_HANDLE)) == FILE_TYPE_CHAR;
+    return GetFileType (GetStdHandle (STD_OUTPUT_HANDLE)) == FILE_TYPE_CHAR;
 #else
-    return isatty(STDOUT_FILENO);
-#endif // _WIN32
+    return isatty (STDOUT_FILENO);
+#endif	  // _WIN32
   }
 };
 
 const Starter Starter::starter{};
 
-} // namespace detail
+}    // namespace detail
 
-#endif // SCOREBOARD_UTILS_HPP
+#endif	  // SCOREBOARD_UTILS_HPP
+
